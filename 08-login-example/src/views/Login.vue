0a43c65f923b743439a985bc90423a1f90824972
zhuzhiou <zhuzhiou@qq.com>
2018-03-23 19:07:07 +0800

    提交代码

---
+++ 08-login-example/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-form>
+    <el-form-item label="账号">
+      <el-input type="text" v-model="login.username"></el-input>
+      <p>{{login.usernameError}}</p>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary">登陆</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      login: {
+        username: '',
+        usernameError: '请输入账号'
+      }
+    }
+  }
+}
+</script>
