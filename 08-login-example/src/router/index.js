506bbd952e992ebc5f7914fe94d0b3d986d7ee26
zhuzhiou <zhuzhiou@qq.com>
2018-03-23 23:04:06 +0800

    认证拦截器

--- 08-login-example/src/router/index.js
+++ 08-login-example/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -19,3 +19,14 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach(function (to, from, next) {
+  let token = sessionStorage.getItem('token')
+  if (token == null && to.path !== '/login') {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
