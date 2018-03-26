6ddbaecfdb00f28b6b5ec1443bca7f7e59bb0582
zhuzhiou <zhuzhiou@qq.com>
2018-03-26 22:25:29 +0800

    提交示例

--- 08-login-example/src/views/layout/index.vue
+++ 08-login-example/src/views/layout/index.vue
@@ -7,23 +7,32 @@
     </el-header>
     <el-container>
       <el-aside width="200px">
-        <el-menu router default-active="/examples/form">
+        <el-menu router default-active="/admin/form" :unique-opened="true">
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-setting"></i>
-              <span>组件的使用</span>
+              <span>系统设置</span>
             </template>
             <el-menu-item-group>
               <template slot="title">常用组件</template>
-              <el-menu-item index="/admin/room">房间管理</el-menu-item>
-              <el-menu-item index="/admin/table">表格</el-menu-item>
-              <el-menu-item index="/admin/form">表单</el-menu-item>
+              <el-menu-item index="/admin/room">娃娃机管理</el-menu-item>
+              <el-menu-item index="/admin/table">货架管理</el-menu-item>
+              <el-menu-item index="/admin/form">APP管理</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group>
-              <template slot="title">对话框</template>
-              <el-menu-item index="1-4">选项3</el-menu-item>
+              <template slot="title">游戏</template>
+              <el-menu-item index="1-4">游戏列表</el-menu-item>
               <el-menu-item index="1-5">选项4</el-menu-item>
             </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span>运营管理</span>
+            </template>
+            <el-menu-item index="2-1">功能一</el-menu-item>
+            <el-menu-item index="2-2">功能二</el-menu-item>
+            <el-menu-item index="2-3">功能三</el-menu-item>
           </el-submenu>
       </el-menu>
     </el-aside>
